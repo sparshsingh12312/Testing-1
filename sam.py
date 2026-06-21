69dae8041cfd5787a4e3f9b7341f7c8354fae41c
sparsh12312 <sparshsinghop9000@gmail.com>
2026-06-21 14:16:56 +0530

    fourth commit

--- sam.py
+++ sam.py
@@ -1,1 +1,8 @@
-print("Hello World")
+principal=float(input("enter th principal amount"))
+rate=float(input("enter the rate of interest"))
+time=float(input("enter the time duration"))
+amount=principal*(1+rate/100)**time
+print("amount is",round(amount))
+
+ci=amount-principal
+print("compound interest is",round(ci))
